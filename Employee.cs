19177b7e8a34fcb7992b8f829f0935193ac1473b
CaoAnhDuc <Anhduck2208@gmail.com>
2024-11-05 13:02:39 +0700

    test

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public abstract class Employee
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string SocialSecurityNumber { get; private set; }
+
+    public Employee(string first, string last, string ssn)
+    {
+        FirstName = first;
+        LastName = last;
+        SocialSecurityNumber = ssn;
+    }
+
+    public abstract double Earnings(); // pure virtual
+
+    public virtual void Print() // virtual
+    {
+        Console.WriteLine($"{FirstName} {LastName}\nsocial security number: {SocialSecurityNumber}");
+    }
+}
